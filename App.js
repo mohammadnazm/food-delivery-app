fcfcd2101f0990e923b732ff7b8148e823592daf
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-10-30 01:09:06 +0300

    Wrapping the app in NavigationContainer

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <TailwindProvider>
-      <View style={styles.container}>
-        <Text className="text-red-500">
-          Open up App.js to start working on your app!
-        </Text>
-        <StatusBar style="auto" />
-      </View>
-    </TailwindProvider>
+    <NavigationContainer>
+      <TailwindProvider>
+        <View style={styles.container}>
+          <Text className="text-red-500">
+            Open up App.js to start working on your app!
+          </Text>
+          <StatusBar style="auto" />
+        </View>
+      </TailwindProvider>
+    </NavigationContainer>
   );
 }
 
