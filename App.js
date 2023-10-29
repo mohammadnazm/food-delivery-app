fe13fc5e2343c76106985f742909951b0368f463
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-10-30 01:15:40 +0300

    Creating a native stack navigator

--- App.js
+++ App.js
@@ -3,16 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <View style={styles.container}>
-          <Text className="text-red-500">
-            Open up App.js to start working on your app!
-          </Text>
-          <StatusBar style="auto" />
-        </View>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
