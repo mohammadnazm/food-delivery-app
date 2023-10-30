bb425c364858e4556c0ef2fced7215005238d244
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-10-31 00:42:27 +0300

    add style to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>HomeScreen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "red",
+  },
+});
 
 export default HomeScreen;
