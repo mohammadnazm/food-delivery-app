5854326337a7c2ccf2243dc393c6c0c7f46b30dd
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-11-03 23:18:24 +0300

    upgrade

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,5 @@
-import React, { useLayoutEffect } from "react";
-import { useNavigation } from "@react-navigation/core";
+import React from "react";
 import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
-import { ChevronDownIcon, UserIcon } from "react-native-heroicons/outline";
 
 const styles = StyleSheet.create({
   safeAreaView: {
@@ -12,42 +10,18 @@ const styles = StyleSheet.create({
   },
   view: {
     /* Add styles for View here */
-    flexDirection: "row",
-    paddingBottom: 3,
-    alignItems: "center",
-    marginHorizontal: 4,
-    justifyContent: "space-between",
-    marginLeft: 0, // Add left margin here
   },
   image: {
     /* Add styles for Image here */
-    height: 50, // You can adjust the height as needed
-    width: 50, // You can adjust the width as needed
+    height: 70, // You can adjust the height as needed
+    width: 70, // You can adjust the width as needed
     backgroundColor: "#D1D5DB", // You can adjust the background color as needed
     padding: 10, // You can adjust the padding as needed
     borderRadius: 50, // This creates a circle, adjust as needed
   },
-  deliveryText: {
-    fontWeight: "bold",
-    color: "gray",
-    fontSize: 14,
-  },
-  locationText: {
-    fontWeight: "bold",
-    fontSize: 26,
-    color: "black",
-  },
 });
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
-
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Text style={styles.textRed}>
@@ -57,14 +31,6 @@ const HomeScreen = () => {
             source={{ uri: "https://links.papareact.com/wru" }}
             style={styles.image}
           />
-          <View style={{ marginLeft: 14 }}>
-            <Text style={styles.deliveryText}>Deliver Now!</Text>
-            <Text style={styles.locationText}>
-              Current Location <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color="#00CCBB" />
         </View>
       </Text>
     </SafeAreaView>
--- tailwindt.config.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//   content: [
-//     "./screens/**/*.{js,ts,jsx,tsx}",
-//     "./pages/**/*.{js,ts,jsx,tsx}",
-//     "./components/**/*.{js,ts,jsx,tsx}",
-//   ],
-// };
