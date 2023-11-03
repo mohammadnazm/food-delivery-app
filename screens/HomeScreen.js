f80783723a482ba557638587bf86c3f54cfad304
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-11-03 21:58:20 +0300

    add Image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,40 @@
-import { useNavigation } from "@react-navigation/core";
-import React, { useLayoutEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    /* Add styles for SafeAreaView here */
+  },
+  textRed: {
+    color: "#EF4444", // You can replace this with the appropriate shade of red
+  },
+  view: {
+    /* Add styles for View here */
+  },
+  image: {
+    /* Add styles for Image here */
+    height: 70, // You can adjust the height as needed
+    width: 70, // You can adjust the width as needed
+    backgroundColor: "#D1D5DB", // You can adjust the background color as needed
+    padding: 10, // You can adjust the padding as needed
+    borderRadius: 50, // This creates a circle, adjust as needed
+  },
+});
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
-
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.text}>HomeScreen</Text>
-      </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Text style={styles.textRed}>
+        {/* Header */}
+        <View style={styles.view}>
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            style={styles.image}
+          />
+        </View>
+      </Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  text: {
-    color: "red",
-  },
-});
 
 export default HomeScreen;
