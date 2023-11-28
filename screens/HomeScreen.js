ad2a0177624645702caa2e36498acb318cde54c1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-11-29 00:18:57 +0300

    add more style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,13 +13,18 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500">
+      <Text className="text-red-500 mt-8">
         {/* Header */}
-        <View>
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{ uri: "https://links.papareact.com/wru" }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
+
+          <View>
+            <Text>Deliver Now!</Text>
+            <Text>Current Location!</Text>
+          </View>
         </View>
       </Text>
     </SafeAreaView>
