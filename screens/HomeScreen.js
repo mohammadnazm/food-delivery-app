bf0882d92eb06e6cd897420f917e39dd65f075f9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-11-29 00:12:32 +0300

    add style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import { SafeAreaView, Text, View, Image } from "react-native";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <Text>
+      <Text className="text-red-500">
         {/* Header */}
         <View>
-          <Image source={{ uri: "https://links.papareact.com/wru" }} />
+          <Image
+            source={{ uri: "https://links.papareact.com/wru" }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
         </View>
       </Text>
     </SafeAreaView>
