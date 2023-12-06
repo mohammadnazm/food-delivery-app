ceab61c613979f1f8c646fdccb7a646d209ff483
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-12-07 01:28:13 +0300

    Building the Header Section of the Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,28 +13,36 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Text className="text-red-500 mt-8">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+    <SafeAreaView className="bg-white">
+      <Text></Text>
+      {/* Header */}
+      <View className="flex-row justify-between items-center mt-4">
+        {/* Left side */}
+        <View className="flex-row items-center">
           <Image
             source={{ uri: "https://links.papareact.com/wru" }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-
-          <View>
+          <View className="ml-2">
             <Text className="font-bold text-gray-400 text-xs">
               Deliver Now!
             </Text>
             <Text className="font-bold text-xl">
-              Current Location!
+              Current Location!{" "}
               <Icon name="chevron-down" size={20} color="#000" />
             </Text>
           </View>
-
-          <Icon name="user" size={30} color="#000" />
         </View>
-      </Text>
+
+        {/* Right side */}
+        <Icon name="user" size={30} color="#000" />
+
+        {/* Search */}
+        <View>
+          <View></View>
+          <Icon name="filter" size={30} color="#000" />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
