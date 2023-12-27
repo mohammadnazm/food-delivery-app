e269386677252488adc09d17f7e8d83f06aed1f7
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-12-27 23:48:35 +0300

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,35 +14,36 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white">
-      <Text></Text>
-      {/* Header */}
-      <View className="flex-row justify-between items-center mt-10">
-        {/* Left side */}
-        <View className="flex-row items-center">
-          <Image
-            source={{ uri: "https://links.papareact.com/wru" }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-          <View className="ml-2">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location!{" "}
-              <Icon name="chevron-down" size={20} color="#000" />
-            </Text>
+      <Text>
+        {/* Header */}
+        <View className="flex-row justify-between items-center mt-10">
+          {/* Left side */}
+          <View className="flex-row items-center">
+            <Image
+              source={{ uri: "https://links.papareact.com/wru" }}
+              className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+            <View className="ml-2">
+              <Text className="font-bold text-gray-400 text-xs">
+                Deliver Now!
+              </Text>
+              <Text className="font-bold text-xl">
+                Current Location!{" "}
+                <Icon name="chevron-down" size={20} color="#000" />
+              </Text>
+            </View>
+          </View>
+
+          {/* Right side */}
+          <Icon name="user" size={30} color="#000" />
+
+          {/* Search */}
+          <View>
+            <View></View>
+            <Icon name="filter" size={30} color="#000" />
           </View>
         </View>
-
-        {/* Right side */}
-        <Icon name="user" size={30} color="#000" />
-
-        {/* Search */}
-        <View>
-          <View></View>
-          <Icon name="filter" size={30} color="#000" />
-        </View>
-      </View>
+      </Text>
     </SafeAreaView>
   );
 };
