c71f79205281537ccd7ca43c52863cfb9b17458f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-01-01 04:53:57 +0300

    Building the Categories of the Body Section (2/2)

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,12 +1,37 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
-const CategoryCard = () => {
+const CategoryCard = ({ imgUrl, title }) => {
   return (
-    <View>
-      <Text>CategoryCard</Text>
-    </View>
+    <TouchableOpacity style={styles.touchableOpacity}>
+      <View style={styles.container}>
+        <Image source={{ uri: imgUrl }} style={styles.image} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  touchableOpacity: {
+    position: "relative",
+    marginRight: 5.8,
+  },
+  container: {
+    // You can add additional styles for the container if needed
+  },
+  image: {
+    width: 75,
+    height: 67,
+    // borderRadius: 25,
+  },
+  title: {
+    position: "absolute",
+    bottom: 1,
+    left: 1,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
 
 export default CategoryCard;
--- components/categories.js
+++ components/categories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, ScrollView } from "react-native";
+import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   return (
@@ -12,7 +13,30 @@ const Categories = () => {
       showsHorizontalScrollIndicator={false}
     >
       {/* Category Card */}
-      <Text>Categories Hello</Text>
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
     </ScrollView>
   );
 };
