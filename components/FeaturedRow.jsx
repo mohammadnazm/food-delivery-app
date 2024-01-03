4e69ba1317a159ef68d121b62da938c3d73bab3a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-01-03 23:54:37 +0300

    Building the Featured Rows of the Body Section

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -32,6 +32,30 @@ const FeaturedRow = ({ id, title, description }) => {
           long={20}
           lat={0}
         />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo ! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main st"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo ! Sushi"
+          rating={4.5}
+          genre="Japanese"
+          address="123 Main st"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
       </ScrollView>
     </View>
   );
