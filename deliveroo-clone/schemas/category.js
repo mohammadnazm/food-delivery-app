62ce7b24f71fecaa8575e9624eb93d251018618e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-01-05 17:34:36 +0300

    Adding And Changing Sanity Schema

--- deliveroo-clone/schemas/category.js
+++ deliveroo-clone/schemas/category.js
@@ -1,19 +1,20 @@
-import {defineField, defineType} from 'sanity'
+import {defineType} from 'sanity'
 
 export default defineType({
   name: 'category',
-  title: 'Category',
+  title: 'Menu Category',
   type: 'document',
   fields: [
-    defineField({
+    {
       name: 'title',
       title: 'Title',
       type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'image',
+      title: 'Image of Category',
+      type: 'image',
+    },
   ],
 })
