5f45e05068086a922f75992642fe8e756d8d7e98
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-01-16 04:25:42 +0300

    Building the Restaurant Info Section in the Restaurant Screen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,7 +1,7 @@
-import { Image, ScrollView, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { ArrowLeftIcon, StarIcon } from "react-native-heroicons/outline";
 import { urlFor } from "../sanity";
 
 const RestaurantScreen = () => {
@@ -42,6 +42,22 @@ const RestaurantScreen = () => {
           >
             <ArrowLeftIcon color="#00CCBB" />
           </TouchableOpacity>
+        </View>
+
+        <View className="bg-white">
+          <View className="px-4 pt-4">
+            <Text className="text-3xl font-bold">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row">
+                <StarIcon color="green" opacity={0.5} size={22} />
+                <Text className="text-xs text-gray-500">
+                  <Text className="text-green-500">
+                    {rating} . {genre}
+                  </Text>
+                </Text>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
     </ScrollView>
